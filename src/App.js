6b754708fb9ea6551ff83d4f9accaf42dc22045a
unknown <william.z.wilson95@gmail.com>
2020-02-04 16:11:35 -0600

    adds a working sign in page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core';
-import PrivateRoute from './utils/PrivateRoute';
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
+import React, { useState } from 'react';
+import Signup from './components/signup';
+import TopBar from "./components/topbar"
 
 function App() {
   return (
     <div className="App">
-      <CssBaseline>
-      <Router>
-        <Navbar />
-        <Switch>
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <Route path="/login" component={Login} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-      </CssBaseline>
+      <TopBar />
+     <Signup />
     </div>
   );
 }
