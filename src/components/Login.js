201837d43b565aceec37ffb810a8a9b8fe746fed
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-05 11:04:56 -0800

    fixes login

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ import axiosWithAuth from '../utils/axiosWithAuth';
       <div>
           <h2>Welcome Back!</h2>
           <Form>
-              <label htmlFor="username">username</label>
+              {/* <label htmlFor="username">username</label>
               <Field
                   id="username"
                   type="text"
@@ -22,7 +22,19 @@ import axiosWithAuth from '../utils/axiosWithAuth';
                   name="username"
               />
                 {touched.username && errors.username && <p 
-                className="errors">{errors.username}</p>}
+                className="errors">{errors.username}</p>} */}
+
+              <label htmlFor="email">email</label>
+              <Field 
+                id="email"
+                type="text"
+                placeholder="email"
+                name="email"
+              />
+              {touched.email && errors.email && (
+                <p className="errors">{errors.email}</p>
+              )}
+
               <label htmlFor="password">password</label>
               <Field
                   id="password"
@@ -42,15 +54,17 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 
 const FormikLogin = withFormik({
 
-  mapPropsToValues({ username, password }){
+  mapPropsToValues({ username, password, email }){
       return {
-          username: username || "",
-          password: password || ""
+          // username: username || "",
+          password: password || "",
+          email: email || ""
       };
   },
   validationSchema: Yup.object().shape({
-      username: Yup.string().required(),
-      password: Yup.string().required()
+      // username: Yup.string().required(),
+      password: Yup.string().required(),
+      email: Yup.string().required()
   }),
   handleSubmit(values, {setStatus, props}){
       console.log("submitting", values);
@@ -58,10 +72,10 @@ const FormikLogin = withFormik({
       axiosWithAuth()
         .post('/auth/login', values)
         .then(res => {
-          console.dir('success', res)
+          console.dir(res)
           setStatus(res.data)
 
-          localStorage.setItem('token', res.data);
+          localStorage.setItem('token', res.data.message);
           props.history.push('/dashboard');
       })
         .catch(err => {
