21432a3b9ef2412fdf0b1b4dc6903ca01e9680f8
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-06-21 23:17:38 -0700

    Fix indentation on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,30 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {fetchProjects} from "../actions/projectsActions";
-import {Grid} from "@material-ui/core";
-import ProjectCard from "./ProjectCard";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProjects } from '../actions/projectsActions';
+import { Grid } from '@material-ui/core';
+import ProjectCard from './ProjectCard';
 
 const Home = () => {
-    const projects = useSelector(state => state.projects);
-    const isFetching = useSelector(state => state.isFetching);
-    const dispatch = useDispatch();
+  const projects = useSelector((state) => state.projects);
+  const isFetching = useSelector((state) => state.isFetching);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchProjects());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchProjects());
+  }, [dispatch]);
 
-
-    return (
-        <>
-            <h1>All Projects</h1>
-            {!isFetching && projects && (
-                <Grid container spacing={4}>
-                    {projects.map(project => <ProjectCard key={project.id} project={project} />)}
-                </Grid>
-            )}
-        </>
-    );
+  return (
+    <>
+      <h1>All Projects</h1>
+      {!isFetching && projects && (
+        <Grid container spacing={4}>
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </Grid>
+      )}
+    </>
+  );
 };
 
 export default Home;
