01ac219232ddf0207325b6c9218256ada75e3a09
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-06-21 23:38:55 -0700

    Fix indentation in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,42 +1,42 @@
 // reducers/index.js
 
 import {
-    FETCH_PROJECTS_START,
-    FETCH_PROJECTS_SUCCESS,
-    FETCH_PROJECTS_FAILURE
+  FETCH_PROJECTS_START,
+  FETCH_PROJECTS_SUCCESS,
+  FETCH_PROJECTS_FAILURE,
 } from '../actions/projectsActions';
 
 const initialState = {
-    projects: null,
-    isFetching: false,
-    error: ''
+  projects: null,
+  isFetching: false,
+  error: '',
 };
 
 const projectsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_PROJECTS_START:
-            return {
-                ...state,
-                isFetching: true
-            };
+  switch (action.type) {
+    case FETCH_PROJECTS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
 
-        case FETCH_PROJECTS_SUCCESS:
-            return {
-                ...state,
-                projects: action.payload,
-                isFetching: false
-            };
+    case FETCH_PROJECTS_SUCCESS:
+      return {
+        ...state,
+        projects: action.payload,
+        isFetching: false,
+      };
 
-        case FETCH_PROJECTS_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                isFetching: false
-            };
+    case FETCH_PROJECTS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default projectsReducer;
